73061d42e4f4d476c3e3ed2b83c28aec9f73abe6
Gersón Gómez <ggomez@codemonkey.com.mx>
2019-11-11 14:17:12 -0500

    Update configuration.php

--- configuration.php
+++ configuration.php
@@ -155,7 +155,7 @@ class Configuration
 	 *
      * @var string $db_name
      */
-	public static $db_name = 'gv-web-platform-dev';
+	public static $db_name = 'gv-website-dev';
 
 	/**
      * Usuario de la base de datos
