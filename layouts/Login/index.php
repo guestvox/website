e213647f512a7b43cf4cd7b63eff8bda0be77c17
r0al3x <r0b0f0sn0w@hotmail.com>
2020-02-10 15:39:04 -0600

    Login

--- layouts/Login/index.php
+++ layouts/Login/index.php
@@ -1,13 +1,16 @@
 <?php
 defined('_EXEC') or die;
 
-$this->dependencies->add(['css', '{$path.css}Index/index.css']);
-$this->dependencies->add(['js', '{$path.js}Index/index.js']);
+$this->dependencies->add(['css', '{$path.css}Login/index.css']);
+$this->dependencies->add(['js', '{$path.js}Login/index.js']);
 // $this->dependencies->add(['other', '<script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBLCea8Q6BtcTHwY3YFCiB0EoHE5KnsMUE"></script>']);
 ?>
 
-<main class="landing-page-index">
-    <form name="login">
+
+
+    <section class="modals">
+        <main>
+        <form name="login">
         <figure>
             <img src="{$path.images}icon-color.svg" alt="GuestVox icontype">
         </figure>
@@ -17,7 +20,10 @@ $this->dependencies->add(['js', '{$path.js}Index/index.js']);
         <fieldset>
             <input type="password" name="password" placeholder="{$lang.password}" />
         </fieldset>
-        <a class="btn" data-action="login">{$lang.login}</a>
-        <a href="/">{$lang.cancel}</a>
-    </form>
-</main>
+        <a class="btn btn-login" data-action="login">{$lang.login}</a>
+        <br>
+        <a button-cancel href="/" class="btn-cancel">{$lang.cancel}</a>
+    </form>            
+        </main>
+
+    </section>
