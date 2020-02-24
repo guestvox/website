b4c5cb7c2a0334cb04096bbdb65399c09fdfaaa8
Gersón Gómez <ggomez@codemonkey.com.mx>
2020-02-24 14:54:22 -0500

    Actualización

---
+++ layouts/Hola/reputation.php
@@ -0,0 +1,13 @@
+<?php
+
+defined('_EXEC') or die;
+
+$this->dependencies->add(['css', '{$path.css}Hola/index.css']);
+
+?>
+
+<main class="landing-page-reputation">
+    <section class="">
+
+    </section>
+</main>
