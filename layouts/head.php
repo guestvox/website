a34612c7d5210975c4635ed4375c6a4886e0bff5
r0al3x <r0b0f0sn0w@hotmail.com>
2020-03-09 11:40:35 -0600

    Dependencias de pwa agregadas, testear

--- layouts/head.php
+++ layouts/head.php
@@ -26,8 +26,7 @@
                 <meta name="apple-mobile-web-app-capable" content="yes" />
                 <meta name="apple-mobile-web-app-status-bar-style" content="default" />
                 <link rel="apple-touch-icon" sizes="180x180" href="images/icon_180x180.png" />
-<!--
-                No borrar
+
                 <link rel="apple-touch-startup-image" media="screen and (device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)" href="images/icon_1136x640.png" />
                 <link rel="apple-touch-startup-image" media="screen and (device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3) and (orientation: landscape)" href="images/icon_2436x1125.png" />
                 <link rel="apple-touch-startup-image" media="screen and (device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)" href="images/icon_1792x828.png" />
@@ -48,7 +47,7 @@
                 <link rel="apple-touch-startup-image" media="screen and (device-width: 834px) and (device-height: 1194px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)" href="images/icon_1668x2388.png" />
                 <link rel="apple-touch-startup-image" media="screen and (device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)" href="images/icon_2048x1536.png" />
                 <link rel="apple-touch-startup-image" media="screen and (device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)" href="images/icon_1536x2048.png" />
--->
+
                 <!--Terminan dependencias de pwa para ios-->
                 <!--Terminan dependencias de PWA-->
 		{$dependencies.css}
