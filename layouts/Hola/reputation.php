0794758e96dd78445441f24d617902439214c22e
davidgomezmacias <davidgomezmacias@gmail.com>
2020-03-18 23:43:23 -0500

    Landing page hola/reputacion

--- layouts/Hola/reputation.php
+++ layouts/Hola/reputation.php
@@ -6,72 +6,113 @@ $this->dependencies->add(['css', '{$path.css}Hola/reputation.css']);
 
 ?>
 
-<main class="landing-page-reputation">
-    <section class="st-1">
-        <div>
-            <figure>
-                <img src="{$path.images}hola/reputation/logotype-color.png" alt="Logotype">
+<main class="landingpage-reputation">
+    <section id="section-one" class="gradient hand-background">
+        <div class="container">
+            <figure class="logotype">
+                <img src="{$path.images}hola/reputation/logotype.svg" alt="Guestvox logotipo">
             </figure>
-            <h1>{$lang.st-1-text-1}</h1>
-        </div>
-        <figure>
-            <img src="{$path.images}hola/reputation/st-1-img-1.png" alt="Logotype">
-        </figure>
-        <div>
-            <p>{$lang.st-1-text-2}</p>
+
+            <div class="content">
+                <h3>Conoce la opinión de tus consumidores</h3>
+                <p><span>GuestVox es una plataforma, en la que <strong>sólo tus clientes</strong> pueden contestar tu encuesta de satisfacción y dejar una valoración.</span></p>
+            </div>
         </div>
     </section>
-    <section class="st-2">
-        <div>
-            <div>
-                <div>
-                    <div>
-                        <figure>
-                            <img src="{$path.images}hola/reputation/st-2-img-1.png" alt="Asset">
-                        </figure>
-                        <div>
-                            <p>{$lang.st-2-text-1}</p>
-                        </div>
+
+    <section id="section-two" class="gradient-t">
+        <div class="container">
+            <div class="space50"></div>
+            <div class="row">
+                <div class="span4">
+                    <span class="st-2-img-1"></span>
+                    <div class="box">
+                        <p>Construye confianza y compromiso con tus clientes recibiendo sus opiniones reales.</p>
                     </div>
                 </div>
-                <div>
-                    <div>
-                        <figure>
-                            <img src="{$path.images}hola/reputation/st-2-img-2.png" alt="Asset">
-                        </figure>
-                        <div>
-                            <p>{$lang.st-2-text-2}</p>
-                        </div>
+                <div class="span4">
+                    <span class="st-2-img-2"></span>
+                    <div class="box">
+                        <p>Personaliza y configura tus encuestas de acuerdo a tus necesidades, en el momento que quieras.</p>
                     </div>
                 </div>
-                <div>
-                    <div>
-                        <figure>
-                            <img src="{$path.images}hola/reputation/st-2-img-3.png" alt="Asset">
-                        </figure>
-                        <div>
-                            <p>{$lang.st-2-text-3}</p>
-                        </div>
+                <div class="span4">
+                    <span class="st-2-img-3"></span>
+                    <div class="box">
+                        <p>Obtén en tiempo real, las métricas de las respuestas de tus consumidores y no dejes que se vaya insatisfecho.</p>
                     </div>
                 </div>
             </div>
-        </div>
-        <div>
-            <a>{$lang.st-2-text-4}</a>
+            <div class="space50"></div>
+
+            <a href="#" class="btn">¡Lo quiero!</a>
+
+            <div class="space50"></div>
         </div>
     </section>
-    <section class="st-3">
-        <div>
-            <p>{$lang.st-3-text-1}</p>
-        </div>
-        <div>
-            <div>
-                <h2>{$lang.st-3-text-2}</h2>
-                <p>{$lang.st-3-text-3}</p>
-            </div>
+
+    <section id="section-three">
+        <div class="container">
+            <h3>Aprovecha los comentarios y reseñas de sus usuarios, a través de nuestras herramientas.</h3>
             <figure>
-                <img src="{$path.images}hola/reputation/st-3-img-1.png" alt="Asset">
+                <img src="{$path.images}hola/reputation/st-3-img-1.png" alt="Comentarios">
             </figure>
+        </div>
+
+        <span class="st-2-img-1"></span>
+    </section>
+
+    <section id="section-four">
+        <div class="container">
+            <h3>Identifica las necesidades de tus clientes para maximizar su experiencia y convertirlos en clientes frecuentes.</h3>
+        </div>
+
+        <span>Comentarios reales, <br>son cambios reales.<span class="st-2-img-1"></span></span>
+    </section>
+
+    <section id="section-five">
+    </section>
+
+    <section id="section-six">
+        <div class="container">
+            <h3>Decide la cantidad de preguntas y subpreguntas que requieres, modifícalas sin restricciones en cualquier momento.</h3>
+        </div>
+    </section>
+
+    <section id="section-seven">
+    </section>
+
+    <section id="section-eight">
+        <div class="container">
+            <h3>Identifica tus áreas de oportunidad y haz inteligencia de negocio.</h3>
+
+            <p><span>Convierte</span> Convierte los datos en mejoras del servicio</p>
+        </div>
+    </section>
+
+    <section id="section-nine" class="gradient-c-r">
+        <div class="container">
+            <div class="content">
+                <img src="https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=(Poner texto o link con https...)" alt="">
+                <h4>Contesta nuestra encuesta.</h4>
+                <p>*Si pones puntuación negativa o menos de 3, se despliega pregunta secundaria.</p>
+            </div>
+            <h3>VIVE NUESTRA <strong>EXPERIENCIA</strong></h3>
+        </div>
+    </section>
+
+    <section id="section-ten" class="gradient-c-l">
+        <div class="container">
+            <div class="content">
+                <a href="#" class="social-media">F</a>
+                <a href="#" class="social-media">F</a>
+                <a href="#" class="social-media">F</a>
+                <a href="#" class="social-media">F</a>
+            </div>
+            <div class="content">
+                <h3>Quiero agendar una demo</h3>
+                <a href="#" class="btn">¡Agendar ahora!</a>
+            </div>
         </div>
     </section>
 </main>
