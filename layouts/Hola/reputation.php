1bb3f40922b2444fe17fadd70a74db70c72ddfe9
davidgomezmacias <davidgomezmacias@gmail.com>
2020-03-19 12:50:44 -0500

    Final de landingpage reputation

--- layouts/Hola/reputation.php
+++ layouts/Hola/reputation.php
@@ -23,26 +23,28 @@ $this->dependencies->add(['css', '{$path.css}Hola/reputation.css']);
     <section id="section-two" class="gradient-t">
         <div class="container">
             <div class="space50"></div>
-            <div class="row">
-                <div class="span4">
+
+            <div class="content">
+                <div>
                     <span class="st-2-img-1"></span>
                     <div class="box">
                         <p>Construye confianza y compromiso con tus clientes recibiendo sus opiniones reales.</p>
                     </div>
                 </div>
-                <div class="span4">
+                <div>
                     <span class="st-2-img-2"></span>
                     <div class="box">
                         <p>Personaliza y configura tus encuestas de acuerdo a tus necesidades, en el momento que quieras.</p>
                     </div>
                 </div>
-                <div class="span4">
+                <div>
                     <span class="st-2-img-3"></span>
                     <div class="box">
                         <p>Obtén en tiempo real, las métricas de las respuestas de tus consumidores y no dejes que se vaya insatisfecho.</p>
                     </div>
                 </div>
             </div>
+
             <div class="space50"></div>
 
             <a href="#" class="btn">¡Lo quiero!</a>
@@ -71,6 +73,17 @@ $this->dependencies->add(['css', '{$path.css}Hola/reputation.css']);
     </section>
 
     <section id="section-five">
+        <div class="container">
+            <h5>Personaliza</h5>
+            <div>
+                <img src="{$path.images}hola/reputation/pantalla_ipad.png" alt="iPad">
+                <div class="content">
+                    <h3>Nuestra herramienta es totalmente autogestionable.</h3>
+
+                    <a href="#" class="btn">¡Agendar ahora!</a>
+                </div>
+            </div>
+        </div>
     </section>
 
     <section id="section-six">
@@ -80,6 +93,11 @@ $this->dependencies->add(['css', '{$path.css}Hola/reputation.css']);
     </section>
 
     <section id="section-seven">
+        <div class="container">
+            <h3>Visualiza las métricas y datos generados de las encuestas realizadas por tus clientes y toma acciones concretas para mejorar el servicio.</h3>
+
+            <img src="{$path.images}hola/reputation/graficas.png" alt="graficas">
+        </div>
     </section>
 
     <section id="section-eight">
@@ -104,14 +122,18 @@ $this->dependencies->add(['css', '{$path.css}Hola/reputation.css']);
     <section id="section-ten" class="gradient-c-l">
         <div class="container">
             <div class="content">
-                <a href="#" class="social-media">F</a>
-                <a href="#" class="social-media">F</a>
-                <a href="#" class="social-media">F</a>
-                <a href="#" class="social-media">F</a>
-            </div>
-            <div class="content">
                 <h3>Quiero agendar una demo</h3>
                 <a href="#" class="btn">¡Agendar ahora!</a>
+            </div>
+            <div class="content">
+                <figure class="logotype">
+                    <img src="{$path.images}hola/reputation/logotype.svg" alt="Guestvox logotipo">
+                </figure>
+
+                <a href="#" class="social-media fb-logo" target="_blank">FB</a>
+                <a href="#" class="social-media yt-logo" target="_blank">YT</a>
+                <a href="#" class="social-media ig-logo" target="_blank">IG</a>
+                <a href="#" class="social-media in-logo" target="_blank">IN</a>
             </div>
         </div>
     </section>
