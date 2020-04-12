16b713a96f89850b04a52310e3fe6657054d727b
Gersón Gómez <ggomez@codemonkey.com.mx>
2020-04-12 10:46:30 -0500

    Actualización de dashboard

--- layouts/Dashboard/index.php
+++ layouts/Dashboard/index.php
@@ -3,13 +3,11 @@
 defined('_EXEC') or die;
 
 $this->dependencies->add(['js', '{$path.js}Dashboard/index.js']);
-$this->dependencies->add(['js', '{$path.plugins}moment/moment.min.js']);
-$this->dependencies->add(['js', '{$path.plugins}moment/moment-timezone-with-data.min.js']);
 $this->dependencies->add(['other', '<script>menu_focus("dashboard");</script>']);
 
 ?>
 
 %{header}%
 <main class="dashboard">
-    
+
 </main>
