7945b2b0a7387962346aa9e7cf8551e18b710d9c
Gersón Gómez <ggomez@codemonkey.com.mx>
2020-05-29 01:15:28 -0500

    Actualización

--- core/controllers/Dashboard_controller.php
+++ core/controllers/Dashboard_controller.php
@@ -4,9 +4,13 @@ defined('_EXEC') or die;
 
 class Dashboard_controller extends Controller
 {
+	private $lang;
+
 	public function __construct()
 	{
 		parent::__construct();
+
+		$this->lang = Session::get_value('account')['language'];
 	}
 
 	public function index()
