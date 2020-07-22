b1b02e607490f550d5bf64ee84d6cc06ea27b304
Saul Poot <saulantonio219@gmail.com>
2020-07-22 10:46:54 -0500

    Actualización

--- configuration.php
+++ configuration.php
@@ -139,7 +139,7 @@ class Configuration
      * @var string $db_host
      */
 	// public static $db_host = 'guestvox.com';
-	public static $db_host = 'guestvox.com';
+	public static $db_host = 'localhost';
 
 	/**
      * Nombre de la base de datos
@@ -158,7 +158,7 @@ class Configuration
      * @var string $db_user
      */
 	// public static $db_user = 'guestvox';
-	public static $db_user = 'guestvox';
+	public static $db_user = 'root';
 
 	/**
      * Contraseña de la base de datos.
@@ -168,7 +168,7 @@ class Configuration
      * @var string $db_pass
      */
 	// public static $db_pass = 'Jsw90w&6';
-	public static $db_pass = 'Jsw90w&6';
+	public static $db_pass = '';
 
 	/**
      * Tipo de caracteres para la base de datos. Opcional.
