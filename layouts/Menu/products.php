df0836aa9026e12e4cc53543be0e2a67a789a7bc
Saul Poot <saulantonio219@gmail.com>
2020-09-02 13:17:07 -0500

    Actualizacion

--- layouts/Menu/products.php
+++ layouts/Menu/products.php
@@ -3,7 +3,7 @@
 defined('_EXEC') or die;
 
 $this->dependencies->add(['js', '{$path.js}Menu/products.js']);
-$this->dependencies->add(['js', '{$path.plugins}push/push.min.js']);
+$this->dependencies->add(['other', '{$path.plugins}push/push.js']);
 $this->dependencies->add(['other', '<script>menu_focus("menu");</script>']);
 $this->dependencies->add(['other', '<script src="https://js.pusher.com/7.0/pusher.min.js"></script>']);
 
