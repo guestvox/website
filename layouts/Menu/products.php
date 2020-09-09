48651067b7735af8fb2d25f90b0c3bdc30a0dec9
Saul Poot <saulantonio219@gmail.com>
2020-09-09 11:58:58 -0500

    Actualizacion

--- layouts/Menu/products.php
+++ layouts/Menu/products.php
@@ -4,8 +4,8 @@ defined('_EXEC') or die;
 
 $this->dependencies->add(['js', '{$path.js}Menu/products.js']);
 $this->dependencies->add(['other', '<script>menu_focus("menu_products");</script>']);
-$this->dependencies->add(['js', '{$path.plugins}push/OneSignalSDKWorker.js']);
-$this->dependencies->add(['js', '{$path.plugins}push/OneSignalSDKUpdaterWorker.js']);
+$this->dependencies->add(['js', '/OneSignalSDKWorker.js']);
+$this->dependencies->add(['js', '/OneSignalSDKUpdaterWorker.js']);
 $this->dependencies->add(['other', '<script src="https://js.pusher.com/7.0/pusher.min.js"></script>']);
 
 ?>
