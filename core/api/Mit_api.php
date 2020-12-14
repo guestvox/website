664c1fca15a1c00cc2cf9314cb59248d3e26283f
Gersón Gómez <ggomez@codemonkey.com.mx>
2020-12-14 10:47:22 -0500

    Actualización

--- core/api/Mit_api.php
+++ core/api/Mit_api.php
@@ -9,8 +9,20 @@ class Mit_api extends Model
 
     public function post($params)
     {
+        $archivo = fopen("log.txt","a") or die("Error al crear");
+        fwrite($archivo,"----------------------\n");
+        fwrite($archivo,$_REQUEST['strResponse']);
+        fwrite($archivo,"----------------------\n");
+        fwrite($archivo,"Respuesta en claro ----------------------\n\n");
+        $respuesta = AESCrypto::decrypt($_REQUEST['strResponse'], 'AC1D8AC2C3F14F713B13A82A91D806C3');
+        fwrite($archivo,$respuesta);
+        fwrite($archivo,"----------------------\n");
+
+        echo("Se creo correctamente el log");
+        fclose($archivo);
+
         $query = $this->database->insert('mit', [
-			'code' => json_encode($_POST['strResponse'])
+			'code' => json_encode($respuesta)
 		]);
 
         if (!empty($query))
